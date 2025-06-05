703ceea202fec9b3a1e41c78a0d776e22d64ceb5
flavioferreira <flaviosf.dev@gmail.com>
2025-06-05 19:42:41 -0300

    adiciona views para criar e editar posts dentro do site

--- blog/models.py
+++ blog/models.py
@@ -15,5 +15,8 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def get_published_date(self):
+        return self.published_date
+
     def __str__(self):
         return self.title
