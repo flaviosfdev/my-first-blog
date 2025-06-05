c95e257d94e3877080420c5dbd2b08cd785797af
flavioferreira <flaviosf.dev@gmail.com>
2025-06-04 22:42:46 -0300

    adiciona view e template para tela de detalhes de post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path(route = '', view = views.post_list, name = 'post_list'),
+    path(route = 'post/<int:pk>/', view = views.post_detail, name = 'post_detail'),
 ]
